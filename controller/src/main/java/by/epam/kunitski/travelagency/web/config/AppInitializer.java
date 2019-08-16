1afc950bdb6e8fe4e8e7faa0b5d5e839c04d7012
Yury Kunitski <yury_kunitski@epam.com>
2019-08-16 17:33:45 +0300

    add login.html page and userController. Start see info about spring security. Done config

--- controller/src/main/java/by/epam/kunitski/travelagency/web/config/AppInitializer.java
+++ controller/src/main/java/by/epam/kunitski/travelagency/web/config/AppInitializer.java
@@ -11,7 +11,7 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[]{AppConfig.class};
+        return new Class<?>[]{AppConfig.class, SecurityConfig.class};
     }
 
     @Override
---
+++ controller/src/main/java/by/epam/kunitski/travelagency/web/controller/UserController.java
@@ -0,0 +1,36 @@
+package by.epam.kunitski.travelagency.web.controller;
+
+import by.epam.kunitski.travelagency.dao.entity.User;
+import by.epam.kunitski.travelagency.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/perform_login")
+    public String logIn(ModelMap model) {
+
+
+        return "login";
+    }
+
+    @PostMapping("/perform_login")
+    public String logInForm(@Valid @ModelAttribute("user") User user,
+                           BindingResult result, ModelMap model) {
+
+
+        return "login";
+    }
+
+}
