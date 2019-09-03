2c819ec1c512c3816f1e36adce3cd7942f02fcc6
YuryKunitski <yura.kunickij@gmail.com>
2019-09-03 22:11:57 +0300

    add test method for updateCountryView()

--- controller/src/test/java/by/epam/kunitski/travelagency/web/controller/CountryControllerTest.java
+++ controller/src/test/java/by/epam/kunitski/travelagency/web/controller/CountryControllerTest.java
@@ -16,7 +16,9 @@ import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -27,52 +29,74 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = {AppInitializer.class})
 public class CountryControllerTest {
 
-    @Mock
-    private CountryService countryService;
+  @Mock
+  private CountryService countryService;
 
-    @InjectMocks
-    private CountryController countryController;
+  @InjectMocks
+  private CountryController countryController;
 
-    private MockMvc mockMvc;
+  private MockMvc mockMvc;
+  private Country country = new Country();
 
-    @Before
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders.standaloneSetup(countryController).build();
-    }
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    mockMvc = MockMvcBuilders.standaloneSetup(countryController).build();
+  }
 
-    @Test
-    public void addCountryView() {
-    }
+  @Test
+  public void addCountryView() {
+  }
 
-    @Test
-    public void addCountry() {
-    }
+  @Test
+  public void addCountry() {
 
-    @Test
-    public void viewAllCountries() throws Exception {
-        List<Country> countryList = new ArrayList<>();
-        countryList.add(new Country());
-        countryList.add(new Country());
+  }
 
-        when(countryService.findAll()).thenReturn(countryList);
+  @Test
+  public void viewAllCountries() throws Exception {
+    List<Country> countryList = new ArrayList<>();
+    countryList.add(new Country());
+    countryList.add(new Country());
 
-        mockMvc.perform((RequestBuilder) get("/view_all_countries"))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(view().name("allContries"))
-                .andExpect(model().attribute("countries", countryList));
-    }
+    when(countryService.findAll()).thenReturn(countryList);
 
-    @Test
-    public void updateCountryView() {
-    }
+    mockMvc.perform((RequestBuilder) get("/view_all_countries"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(view().name("allContries"))
+            .andExpect(model().attribute("countries", countryList));
+  }
 
-    @Test
-    public void updateCountry() {
-    }
+  @Test
+  public void updateCountryView() throws Exception {
 
-    @Test
-    public void removeCountry() {
-    }
+    country.setId(1);
+    country.setName("China");
+
+    when(countryService.findById(1)).thenReturn(Optional.of(country));
+
+    mockMvc.perform(get("/update_country_view").param("country_id", "1"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("addCountry"))
+            .andExpect(model().attribute("country_id", 1));
+  }
+
+  @Test
+  public void updateCountry() throws Exception {
+
+    country.setId(1);
+    country.setName("China");
+
+    when(countryService.update(country)).thenReturn(new HashSet<>());
+
+    mockMvc.perform(post("/update_country").param("country_id", "1"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("redirect:/profile_admin?country_updated"));
+
+  }
+
+  @Test
+  public void removeCountry() {
+  }
 }
